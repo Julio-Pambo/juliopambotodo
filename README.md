aecbadbda0ac237e06cdf564f9d57baa7278fb59
juliopambo <juliopambo2@gmail.com>
2023-01-28 20:36:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# juliopambotodo
+ Teste 02 do Modulo 03, do curso Web e Mobile 7ed Explicador Inc - Aplicativo Todo - do video do dev Danny Connell
---
+++ src/pages/Help.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Ajuda</h5>
+    <p>Este aplicativo foi desenvolvido usando Quasar Framework. A funcionalidade do aplicativo é
+    adicionar tarefas, uma lista de tarefas de casa ou uma lista de exercícios de Física por resolver.
+    Apresenta um campo para digitar a tarefa e de seguuida clicar no botão ENTER ou clicar no botão com o
+    sinal (mais), para adicionar na lista.
+    </p>
+
+    <p>Tem a opção para eliminar a tarefa depois de conluída, e um efeito de rasurado com cor cinzenta
+     sobre a tarefa feita. Quando não há tarefas, apresenta uma mensagem. Este aplicativo foi desenvolvido
+     acompanhando passo a passo o video do dev Danny Connell, pelo Youtube. Uma tarefa dada pelo professor,
+     pois é um trabalho do Módulo 03 do curso de Web e Mobile 7ed da maior e melhor EduTech de Mozambique,
+     a Explicador Inc. 
+     </p>
+
+     <p>dev. Julio Pambo</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
